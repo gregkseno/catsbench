f8f561a6f068d5dea8f4a45cc5fa55065c383392
Grigoriy Ksenofontov <gregkseno@gmail.com>
2025-07-19 00:32:05 +0300

    Done benchmark lightning modules

---
+++ src/data/benchmark.py
@@ -0,0 +1,106 @@
+from typing import Any, Dict, Optional, Tuple
+
+from torch.utils.data import Dataset, DataLoader
+from lightning import LightningDataModule
+from src.utils import CoupleDataset, make_infinite_dataloader
+from src.benchmark.benchmark import BenchmarkDiscreteEOT
+
+
+class BenchmarkDataModule(LightningDataModule):
+    def __init__(
+        self,
+        benchmark_config: Dict[Any],
+        train_val_test_split: Tuple[float, float, float],
+        batch_size: int,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ) -> None:
+        assert len(train_val_test_split) == 3, ( 
+            "train_val_test_split must be a tuple of three floats "
+            "representing the proportions for train, val, and test sets."
+        )
+        assert sum(train_val_test_split) == 1.0, \
+            "The sum of train_val_test_split must be equal to 1.0."
+
+        super().__init__()
+        # somehow this function is able to load all 
+        # the method arguments and put to `self.hparams`
+        self.save_hyperparameters(logger=False)
+
+        self.benchmark: Optional[BenchmarkDiscreteEOT] = None
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+        
+        # will be divided by the number of devices in `setup`
+        self.batch_size_per_device = batch_size 
+
+    def prepare_data(self) -> None:
+        pass
+        # BenchmarkDiscreteEOT.download(**self.benchmark.config)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data by seting variables: `self.data_train`, `self.data_val`, `self.data_test`."""
+        assert self.trainer.limit_train_batches > 1, '`self.trainer.limit_train_batches` must be set since the dataloaders are infinite!'
+        # dividing here because the `trainer` is not available in the constructor
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        # here is an `if` because the `setup` method is called multiple times 
+        # for trainer.fit, trainer.validate, trainer.test, etc.
+        if not self.benchmark and not self.data_train and not self.data_val and not self.data_test:
+            self.benchmark = BenchmarkDiscreteEOT(self.hparams.benchmark_config)
+
+            ###################### TRAINING DATASET ######################
+            size_train = int(self.hparams.num_samples * self.hparams.train_val_test_split[0])
+            self.data_train = CoupleDataset(
+                input_dataset=self.benchmark.input_dataset[:size_train],
+                target_dataset=self.benchmark.target_dataset[:size_train],
+            )
+
+            ####################### VALIDATION DATASET ######################
+            size_val = int(self.hparams.num_samples * self.hparams.train_val_test_split[0])
+            self.data_val = CoupleDataset(
+                input_dataset=self.benchmark.input_dataset[:size_val],
+                target_dataset=self.benchmark.target_dataset[:size_val],
+            )
+            ######################### TEST DATASET #########################
+            size_test = int(self.hparams.num_samples * self.hparams.train_val_test_split[0])
+            self.data_test = CoupleDataset(
+                input_dataset=self.benchmark.input_dataset[:size_test],
+                target_dataset=self.benchmark.target_dataset[:size_test]
+            )
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader."""
+        return make_infinite_dataloader(DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        ))
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader."""
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader."""
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
---
+++ src/utils/logging/benchmark.py
@@ -0,0 +1,216 @@
+from typing import Any, Dict, Literal, Optional, Tuple
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from prince import MCA
+
+from lightning.pytorch import Callback, Trainer, LightningModule
+from lightning.pytorch.utilities import rank_zero_only
+
+from src.utils import convert_to_numpy, fig2img
+from src.metrics.contingency_similarity import ContingencySimilarity
+from src.metrics.tv_complement import TVComplement
+
+
+class BenchmarkLogger(Callback):
+    def __init__(
+        self,
+        dim: int,
+        num_trajectories: int, 
+        num_translations: int,
+        axlim: Optional[Tuple[float, float]] = None,
+        samples_figsize: Optional[Tuple[int, int]] = None,
+        trajectories_figsize: Optional[Tuple[int, int]] = None,
+        dpi: int = 100
+    ):
+        super().__init__()
+        self.num_trajectories = num_trajectories
+        self.num_translations = num_translations
+        if dim > 2:
+            self.mca = MCA(n_components=2)
+
+        self.tv_complement = TVComplement()
+        self.contingency_similarity = ContingencySimilarity()
+
+        self.axlim = [7, 93] if axlim is None else axlim
+        self.samples_fig_config = {
+            'figsize': (12, 4) if samples_figsize is None else samples_figsize,
+            'dpi': dpi,
+        }
+        self.samples_start_config = {
+            'label': r'$p_{start}$', 'c': 'g', 's': 35, 'edgecolor': 'black'
+        }
+        self.samples_end_config = {
+            'label': r'$p_{end}$', 'c': 'orange', 's': 35, 'edgecolor': 'black'
+        }
+        self.samples_pred_config = {
+            'label': r'$p_{\theta}$', 'c': 'yellow', 's': 35, 'edgecolor': 'black'
+        }
+        self.trajectories_fig_config = {
+            'figsize': (8, 8) if trajectories_figsize is None else trajectories_figsize,
+            'dpi': dpi,
+        }
+        self.trajectories_pred_config = {
+            'c': 'salmon', 's': 120, 'edgecolors': 'black', 
+            'label': 'Fitted distribution', 'zorder': 1, 'linewidth': 0.8
+        }
+        self.trajectories_start_config = {
+            'c': 'lime', 's': 180, 'edgecolors': 'black', 
+            'label': r'Trajectory start ($x \sim p_0$)', 'zorder': 3
+        }
+        self.trajectories_end_config = {
+            'c': 'yellow', 's': 100, 'edgecolors': 'black', 
+            'label': r'Trajectory end (fitted)', 'zorder': 3
+        }
+        self.trajectory_lines_config = {
+            'back': {'c': 'black', 'markeredgecolor': 'black', 'linewidth': 2, 'zorder': 2},
+            'front': {'c': 'grey', 'markeredgecolor': 'black', 'linewidth': 1, 'zorder': 2}
+        }
+
+    def on_train_batch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        outputs: Dict[str, Any],
+        batch: Tuple[torch.Tensor, torch.Tensor],
+        batch_idx: int,
+    ) -> None:
+        pl_module.eval()
+        x_start, x_end = outputs['x_start'], outputs['x_end']
+
+        if pl_module.current_epoch == 0:
+            self.mca.fit(convert_to_numpy(torch.cat(batch, dim=0)))
+
+        if trainer.is_last_batch:
+            self._log_smaples(x_start, x_end, pl_module, 'train')   
+            self._log_trajectories(x_start, pl_module, stage='train')
+
+    def on_validation_batch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        outputs: Dict[str, Any],
+        batch: Tuple[torch.Tensor, torch.Tensor],
+        batch_idx: int,
+    ) -> None:
+        pl_module.eval()
+        x_start, x_end = outputs['x_start'], outputs['x_end']
+
+        fb = 'forward' if not pl_module.bidirectional or self.current_epoch % 2 == 0 else 'backward'
+        self.tv_complement(x_start, x_end)
+        self.contingency_similarity(x_start, x_end)
+        pl_module.log_dict(
+            {f'val/tv_complement_{fb}': self.tv_complement, 
+             f'val/contingency_similarity_{fb}': self.contingency_similarity}
+        )
+
+        if trainer.is_last_batch:
+            self._log_smaples(x_start, x_end, pl_module, 'val')
+            self._log_trajectories(x_start, pl_module, stage='val')
+
+    def on_test_batch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        outputs: Dict[str, Any],
+        batch: Tuple[torch.Tensor, torch.Tensor],
+        batch_idx: int,
+    ) -> None:
+        pl_module.eval()
+        x_start, x_end = outputs['x_start'], outputs['x_end']
+
+        fb = 'forward' if not pl_module.bidirectional or self.current_epoch % 2 == 0 else 'backward'
+        self.tv_complement(x_start, x_end)
+        self.contingency_similarity(x_start, x_end)
+        pl_module.log_dict(
+            {f'test/tv_complement_{fb}': self.tv_complement, 
+             f'test/contingency_similarity_{fb}': self.contingency_similarity}
+        )
+
+        if trainer.is_last_batch:
+            self._log_smaples(x_start, x_end, pl_module, 'test')
+            self._log_trajectories(x_start, pl_module, stage='test')
+
+    @rank_zero_only
+    def _log_smaples(
+        self,
+        x_start: torch.Tensor | np.ndarray, 
+        x_end: torch.Tensor | np.ndarray, 
+        pl_module: LightningModule,
+        stage: Literal['train', 'val', 'test'] = 'train',
+    ):
+        fb = 'forward' if not pl_module.bidirectional or self.current_epoch % 2 == 0 else 'backward'
+        pred_x_end = self.mca.transform(convert_to_numpy(pl_module.sample(x_start)))
+        x_start = self.mca.transform(convert_to_numpy(x_start))
+        x_end = self.mca.transform(convert_to_numpy(x_end))
+
+        fig, axes = plt.subplots(1, 3, **self.samples_fig_config)
+        fig.suptitle(f'Epoch {pl_module.current_epoch}, Iteration {pl_module.iteration}')
+
+        axes[0].scatter(x_start[:, 0], x_start[:, 1], **self.samples_start_config) 
+        axes[1].scatter(x_end[:, 0], x_end[:, 1], **self.samples_end_config)
+        axes[2].scatter(pred_x_end[:, 0], pred_x_end[:, 1], **self.samples_pred_config) 
+        
+        for i in range(3):
+            axes[i].grid()
+            axes[i].set_xlim(self.axlim)
+            axes[i].set_ylim(self.axlim)
+            axes[i].legend(loc="lower left")
+        fig.tight_layout(pad=0.5)
+        img = fig2img(fig)
+        pl_module.logger.log_image(
+            key=f'{stage}/samples_{fb}', images=[img], step=pl_module.global_step
+        )
+        plt.close()
+
+    @rank_zero_only
+    def _log_trajectories(
+        self,
+        x_start: torch.Tensor | np.ndarray, 
+        pl_module: LightningModule,
+        stage: Literal['train', 'val', 'test'] = 'train',
+    ):
+        fb = 'forward' if not pl_module.bidirectional or self.current_epoch % 2 == 0 else 'backward'
+        fig, ax = plt.subplots(1, 1, **self.trajectories_fig_config)
+        ax.get_xaxis().set_ticklabels([])
+        ax.get_yaxis().set_ticklabels([])
+        fig.suptitle(f'Epoch {pl_module.current_epoch}, Iteration {pl_module.iteration}')
+        
+        pred_x_end = self.mca.transform(convert_to_numpy(pl_module.sample(x_start)))
+        traj_start = x_start[:self.num_trajectories]
+        repeats = [self.num_translations] + [1] * traj_start.dim()
+        traj_start = traj_start.unsqueeze(0).repeat(*repeats)
+        traj_start = traj_start.reshape(-1, *x_start.shape[1:])
+        trajectories = pl_module.sample_trajectory(traj_start)
+
+        # Reduce number of timesteps for visualization
+        num_timesteps = trajectories.shape[0]
+        if num_timesteps > 10:
+            trajectories = torch.stack([
+                    trajectories[0], 
+                    trajectories[num_timesteps // 8], 
+                    trajectories[num_timesteps // 2], 
+                    trajectories[(num_timesteps * 7) // 8], 
+                    trajectories[-1]
+                ], dim=0
+            )
+        trajectories = self.mca.transform(convert_to_numpy(trajectories))
+
+        ax.scatter(pred_x_end[:, 0], pred_x_end[:, 1], **self.trajectories_pred_config)
+        ax.scatter(trajectories[0, :, 0], trajectories[0, :, 1], **self.trajectories_start_config)
+        ax.scatter(trajectories[-1, :, 0], trajectories[-1, :, 1], **self.trajectories_end_config)
+        for i in range(self.num_trajectories * self.num_translations):
+            ax.plot(trajectories[:, i, 0], trajectories[:, i, 1], **self.trajectory_lines_config['back'])
+            ax.plot(
+                trajectories[:, i, 0], trajectories[:, i, 1], **self.trajectory_lines_config['front'], 
+                label='Intermediate predictions' if i == 0 else ''
+            )
+        ax.legend(loc='lower left')
+        ax.set_xlim(self.axlim)
+        ax.set_ylim(self.axlim)
+        fig.tight_layout(pad=0.5)
+        img = fig2img(fig)
+        pl_module.logger.log_image(
+            key=f'{stage}/trajectories_{fb}', images=[img], step=pl_module.global_step
+        )
+        plt.close()
