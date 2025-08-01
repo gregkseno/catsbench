edaf7911698db33f26668e215e441c1a037efad5
Grigoriy Ksenofontov <gregkseno@gmail.com>
2025-08-01 22:42:39 +0300

    Add params

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=train-toy
 #SBATCH --partition=ais-gpu
+#SBATCH --reservation=HPC-2665
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=12:00:00
+#SBATCH --time=16-00:00:00
 
 source activate dot_bench
-python -m src.run experiment=csbm_benchmark
+python -m src.run experiment=light_sb_d_benchmark +trainer.max_epochs=10 \
+    data.dim=16 \
+    prior.prior_type=uniform \
+    prior.alpha=0.005
