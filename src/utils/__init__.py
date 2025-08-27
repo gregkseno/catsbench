c12b297e2bc7a53be366f0ce6107506ad2b30be0
Grigoriy Ksenofontov <gregkseno@gmail.com>
2025-08-27 17:34:56 +0300

    Add logger to quick import

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -12,3 +12,4 @@ from src.utils.data import (
 from src.utils.visualization import fig2img
 from src.utils.logging.toy import ToyLogger
 from src.utils.logging.benchmark import BenchmarkLogger
+from src.utils.logging.benchmark_images import BenchmarkImagesLogger
