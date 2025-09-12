977e50ff1e6d60c3e150dcbd6a06e5f99c8a3390
LeonMeon <leonoff_99@mail.ru>
2025-09-12 18:59:23 +0300

    fix syntax error

--- src/utils/data.py
+++ src/utils/data.py
@@ -18,8 +18,9 @@ def log_space_product(log_matrix1: torch.Tensor, log_matrix2: torch.Tensor) -> t
 
 def logits_prod(log_matrix1: torch.Tensor, log_matrix2: torch.Tensor) -> torch.Tensor: 
     log_matrix1 = log_matrix1.unsqueeze(-1) # [batchsize, 3, 32, 32, num_categories, 1]
-    insert_nones = [None] * (log_matrix1.ndim - 3)
-    log_matrix2 = log_matrix2[:, *insert_nones, :, :] # [batchsize, 1, 1, 1, num_categories, num_categories]
+    #insert_nones = [None] * (log_matrix1.ndim - 3)
+    assert (log_matrix1.ndim - 3) == 3, log_matrix1.ndim - 3
+    log_matrix2 = log_matrix2[:, None, None, None, :, :] # [batchsize, 1, 1, 1, num_categories, num_categories]
     return torch.logsumexp(log_matrix1 + log_matrix2, dim=-2)
 
 def convert_to_numpy(x: torch.Tensor | np.ndarray) -> np.ndarray:
