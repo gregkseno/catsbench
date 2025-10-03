18dd23888f4346218f9f7505e7ccb2b155ef5f02
Ark-130994 <xavier13091994@gmail.com>
2025-10-03 13:13:43 +0300

    benchmark_texts

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,4 @@
 from src.data.benchmark import BenchmarkDataModule
 from src.data.toy import ToyDataModule
 from src.data.benchmark_images import BenchmarkImagesDataModule
+from src.data.benchmark_texts import BenchmarkTextsDataModule
