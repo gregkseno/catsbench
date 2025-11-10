Fix order of samplin for kl calculation, remove c2st

src/utils/logging/benchmark.py
@@ -149,8 +149,6 @@ class BenchmarkLogger(Callback):
             },
         )
         pl_module.cond_metrics = pl_module.metrics.clone(prefix='cond_')
-        pl_module.c2st = ClassifierTwoSampleTest(2*self.dim, model='linear')
-        pl_module.cond_c2st = ClassifierTwoSampleTest(2*self.dim, model='linear')
         pl_module.kl_div = KLDivergence(log_prob=True)
 
     def on_train_epoch_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
@@ -188,28 +186,28 @@ class BenchmarkLogger(Callback):
 
         pred_x_end = pl_module.sample(x_start)
         pl_module.metrics.update(x_end, pred_x_end)
-        pl_module.c2st.update(
-            torch.cat([x_start, x_end], dim=-1), 
-            torch.cat([x_start, pred_x_end], dim=-1),
-            train=batch_idx < int(len(trainer.val_dataloaders) * self.train_test_split)
-        )
 
         repeated_x_start = x_start[0].unsqueeze(0).expand(self.num_cond_samples, -1)
         cond_x_end = self.benchmark.sample_target_given_input(repeated_x_start)
         cond_pred_x_end = pl_module.sample(repeated_x_start)
         pl_module.cond_metrics.update(cond_x_end, cond_pred_x_end)
-        pl_module.cond_c2st.update(
-            torch.cat([repeated_x_start, cond_x_end], dim=-1), 
-            torch.cat([repeated_x_start, cond_pred_x_end], dim=-1),
-            train=batch_idx < int(len(trainer.val_dataloaders) * self.train_test_split)
-        )
 
         x = x_start
-        for t in reversed(range(1, pl_module.prior.num_timesteps + 2)):
+        for t in range(0, pl_module.prior.num_timesteps + 1):
             t = torch.tensor([t] * x.shape[0], device=pl_module.device)
-            pred_transition_logits = pl_module.get_transition_logits(x, t, 'forward')
+            # there is must be a better naming for identifying sampling direction
+            # but for now all bidirectional methods use reversed time steps during sampling
+            if pl_module.bidirectional:
+                pred_transition_logits = pl_module.get_transition_logits(
+                    x, pl_module.prior.num_timesteps + 1 - t
+                )
+            else:
+                pred_transition_logits = pl_module.get_transition_logits(x, t)
             true_transition_logits = self.benchmark.get_transition_logits(x, t)
-            pl_module.kl_div.update(true_transition_logits, pred_transition_logits)
+            pl_module.kl_div.update(
+                true_transition_logits.reshape(-1, self.num_categories),
+                pred_transition_logits.reshape(-1, self.num_categories)
+            )
 
             noise = torch.rand_like(pred_transition_logits)
             noise = torch.clamp(noise, min=torch.finfo(noise.dtype).tiny, max=1.)
@@ -223,14 +221,6 @@ class BenchmarkLogger(Callback):
         metrics = {f'val/{k}_{fb}': v for k, v in metrics.items()}
         pl_module.log_dict(metrics)
         pl_module.metrics.reset()
-
-        c2st = pl_module.c2st.compute()
-        pl_module.log(f'val/c2st_{fb}', c2st)
-        pl_module.c2st.reset()
-
-        cond_c2st = pl_module.cond_c2st.compute()
-        pl_module.log(f'val/cond_c2st_{fb}', cond_c2st)
-        pl_module.cond_c2st.reset()
         
         cond_metrics = pl_module.cond_metrics.compute()
         cond_metrics = {f'val/{k}_{fb}': v for k, v in cond_metrics.items()}
@@ -260,28 +250,28 @@ class BenchmarkLogger(Callback):
 
         pred_x_end = pl_module.sample(x_start)
         pl_module.metrics.update(x_end, pred_x_end)
-        pl_module.c2st.update(
-            torch.cat([x_start, x_end], dim=-1), 
-            torch.cat([x_start, pred_x_end], dim=-1),
-            train=batch_idx < int(len(trainer.test_dataloaders) * self.train_test_split)
-        )
         
         repeated_x_start = x_start[0].unsqueeze(0).expand(self.num_cond_samples, -1)
         cond_x_end = self.benchmark.sample_target_given_input(repeated_x_start)
         cond_pred_x_end = pl_module.sample(repeated_x_start)
         pl_module.cond_metrics.update(cond_x_end, cond_pred_x_end)
-        pl_module.cond_c2st.update(
-            torch.cat([repeated_x_start, cond_x_end], dim=-1), 
-            torch.cat([repeated_x_start, cond_pred_x_end], dim=-1),
-            train=batch_idx < int(len(trainer.test_dataloaders) * self.train_test_split)
-        )
 
         x = x_start
-        for t in reversed(range(1, pl_module.prior.num_timesteps + 2)):
+        for t in range(0, pl_module.prior.num_timesteps + 1):
             t = torch.tensor([t] * x.shape[0], device=pl_module.device)
-            pred_transition_logits = pl_module.get_transition_logits(x, t)
+            # there is must be a better naming for identifying sampling direction
+            # but for now all bidirectional methods use reversed time steps during sampling
+            if pl_module.bidirectional:
+                pred_transition_logits = pl_module.get_transition_logits(
+                    x, pl_module.prior.num_timesteps + 1 - t
+                )
+            else:
+                pred_transition_logits = pl_module.get_transition_logits(x, t)
             true_transition_logits = self.benchmark.get_transition_logits(x, t)
-            pl_module.kl_div.update(true_transition_logits, pred_transition_logits)
+            pl_module.kl_div.update(
+                true_transition_logits.reshape(-1, self.num_categories),
+                pred_transition_logits.reshape(-1, self.num_categories)
+            )
 
             noise = torch.rand_like(pred_transition_logits)
             noise = torch.clamp(noise, min=torch.finfo(noise.dtype).tiny, max=1.)
@@ -295,14 +285,6 @@ class BenchmarkLogger(Callback):
         metrics = {f'test/{k}_{fb}': v for k, v in metrics.items()}
         pl_module.log_dict(metrics)
         pl_module.metrics.reset()
-
-        c2st = pl_module.c2st.compute()
-        pl_module.log(f'test/c2st_{fb}', c2st)
-        pl_module.c2st.reset()
-
-        cond_c2st = pl_module.cond_c2st.compute()
-        pl_module.log(f'test/cond_c2st_{fb}', cond_c2st)
-        pl_module.cond_c2st.reset()
 
         cond_metrics = pl_module.cond_metrics.compute()
         cond_metrics = {f'test/{k}_{fb}': v for k, v in cond_metrics.items()}