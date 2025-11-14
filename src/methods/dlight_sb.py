129abc1acd8c3c19dcb711c9154a4bb5011306af
Grigoriy Ksenofontov <gregkseno@gmail.com>
2025-11-14 15:48:11 +0300

    Fix transitions

--- src/methods/dlight_sb.py
+++ src/methods/dlight_sb.py
@@ -219,9 +219,9 @@ class DLightSB(LightningModule):
         tp1 = self.prior.num_timesteps - t_orig
 
         log_u_t = torch.empty(
-            x_t.shape[0], self.num_potentials, self.dim, device=self.device
+            x_t.shape[0], self.hparams.num_potentials, self.hparams.dim, device=self.device
         ) # [B, K, D]
-        for d in range(self.dim):
+        for d in range(self.hparams.dim):
             log_pi_ref_t = self.prior.extract('cumulative', t, row_id=x_t[:, d]) # [B, S]
             log_u_t[:, :, d] = torch.logsumexp(
                 self.log_cp_cores[d][None, :, :] + log_pi_ref_t[:, None, :], dim=-1 # [B, K, S] 
@@ -229,17 +229,17 @@ class DLightSB(LightningModule):
         sum_log_u_t = log_u_t.sum(dim=-1)  # [B, K]
 
         transition_logits = torch.empty(
-            x_t.shape[0], self.dim, self.prior.num_categories, device=self.device
+            x_t.shape[0], self.hparams.dim, self.prior.num_categories, device=self.device
         ) # [B, D, S]
         x_tp1_d = torch.arange(self.prior.num_categories, device=self.device) # [S]
         x_tp1_d = x_tp1_d.unsqueeze(0).repeat(x_t.shape[0], 1).reshape(-1) # [B*S]
         tp1_repeated = tp1.repeat_interleave(self.prior.num_categories) # [B*S]
-        for d in range(self.dim):
+        for d in range(self.hparams.dim):
             log_pi_ref_tp1 = self.prior.extract('cumulative', tp1_repeated, row_id=x_tp1_d) # [B*S, S]
             log_u_tp1_d = torch.logsumexp(
                 self.log_cp_cores[d][None, :, :] + log_pi_ref_tp1[:, None, :], dim=-1 # [B*S, K, S]
             )  # [B*S, K]
-            log_u_tp1_d = log_u_tp1_d.reshape(x_t.shape[0], self.prior.num_categories, self.num_potentials) # [B, S, K]
+            log_u_tp1_d = log_u_tp1_d.reshape(x_t.shape[0], self.prior.num_categories, self.hparams.num_potentials) # [B, S, K]
             log_u_tp1_d = log_u_tp1_d.permute(0, 2, 1) # [B, K, S]      
             log_phi_tp1_d = torch.logsumexp(
                 self.log_alpha[None, :, None] + log_u_tp1_d + (sum_log_u_t - log_u_t[:, :, d])[:, :, None], dim=1 # [B, K, S]
@@ -489,9 +489,9 @@ class DLightSB_old(LightningModule):
         tp1 = self.prior.num_timesteps - t_orig
 
         log_u_t = torch.empty(
-            x_t.shape[0], self.num_potentials, self.dim, device=self.device
+            x_t.shape[0], self.hparams.num_potentials, self.hparams.dim, device=self.device
         ) # [B, K, D]
-        for d in range(self.dim):
+        for d in range(self.hparams.dim):
             log_pi_ref_t = self.prior.extract('cumulative', t, row_id=x_t[:, d]) # [B, S]
             log_u_t[:, :, d] = torch.logsumexp(
                 self.log_cp_cores[d][None, :, :] + log_pi_ref_t[:, None, :], dim=-1 # [B, K, S] 
@@ -499,17 +499,17 @@ class DLightSB_old(LightningModule):
         sum_log_u_t = log_u_t.sum(dim=-1)  # [B, K]
 
         transition_logits = torch.empty(
-            x_t.shape[0], self.dim, self.prior.num_categories, device=self.device
+            x_t.shape[0], self.hparams.dim, self.prior.num_categories, device=self.device
         ) # [B, D, S]
         x_tp1_d = torch.arange(self.prior.num_categories, device=self.device) # [S]
         x_tp1_d = x_tp1_d.unsqueeze(0).repeat(x_t.shape[0], 1).reshape(-1) # [B*S]
         tp1_repeated = tp1.repeat_interleave(self.prior.num_categories) # [B*S]
-        for d in range(self.dim):
+        for d in range(self.hparams.dim):
             log_pi_ref_tp1 = self.prior.extract('cumulative', tp1_repeated, row_id=x_tp1_d) # [B*S, S]
             log_u_tp1_d = torch.logsumexp(
                 self.log_cp_cores[d][None, :, :] + log_pi_ref_tp1[:, None, :], dim=-1 # [B*S, K, S]
             )  # [B*S, K]
-            log_u_tp1_d = log_u_tp1_d.reshape(x_t.shape[0], self.prior.num_categories, self.num_potentials) # [B, S, K]
+            log_u_tp1_d = log_u_tp1_d.reshape(x_t.shape[0], self.prior.num_categories, self.hparams.num_potentials) # [B, S, K]
             log_u_tp1_d = log_u_tp1_d.permute(0, 2, 1) # [B, K, S]      
             log_phi_tp1_d = torch.logsumexp(
                 self.log_alpha[None, :, None] + log_u_tp1_d + (sum_log_u_t - log_u_t[:, :, d])[:, :, None], dim=1 # [B, K, S]
