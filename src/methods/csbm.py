4c23467d554f4455d2f584805b5cace1bb6fc159
Grigoriy Ksenofontov <gregkseno@gmail.com>
2025-12-02 23:25:24 +0300

    Add get_transition_logits method

--- src/methods/csbm.py
+++ src/methods/csbm.py
@@ -281,13 +281,34 @@ class CSBM(LightningModule):
         if 'iteration' in checkpoint:
             self.iteration = checkpoint['iteration']
 
+    @torch.no_grad()
+    def get_transition_logits(
+        self, 
+        x_t: torch.Tensor, 
+        t: torch.Tensor, 
+        fb: Optional[Literal['forward', 'backward']] = None
+    ) -> torch.Tensor:
+        fb = fb or self.fb
+        was_training = self.models[fb].training
+        self.models[fb].eval()
+
+        with self.emas[fb].average_parameters():
+            pred_x_start_logits = self.models[fb](x_t, t)
+
+        if was_training: 
+            self.models[fb].train()
+            
+        pred_transition_logits = self.prior.posterior_logits(pred_x_start_logits, x_t, t, logits=True)
+        return pred_transition_logits
+
     def markov_sample(
         self, 
         x_t: torch.Tensor, 
         t: torch.Tensor, 
         fb: Literal['forward', 'backward'], 
         return_transitions: bool = False
-    ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:            
+    ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        # we don't use here get_transition_logits avoid multiple EMA calls     
         pred_x_start_logits = self.models[fb](x_t, t)
         pred_transition_logits = self.prior.posterior_logits(pred_x_start_logits, x_t, t, logits=True)
         samples = gumbel_sample(
