Fix epsilon calculation in logits methods of Prior class for numerical stability

src/catsbench/prior.py
@@ -236,7 +236,8 @@ class Prior(nn.Module):
         r"""Calculates logits of $p(x_{t-1} | x_{t}, x_{0})$.
         If logits is True, the output is summed over x_0 and transition matrix returned.""" 
         if not logits:
-            x_start_logits = torch.log(torch.nn.functional.one_hot(x_start, self.num_categories) + self.eps)
+            eps = x_start.finfo().eps
+            x_start_logits = torch.log(torch.nn.functional.one_hot(x_start, self.num_categories) + eps)
         else:
             x_start_logits = x_start.clone()
         assert x_start_logits.shape == x_t.shape + (self.num_categories,), \