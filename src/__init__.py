feed2a90c82f13773af1c4cac27bf23677718570
Grigoriy Ksenofontov <gregkseno@gmail.com>
2026-01-22 23:01:58 +0300

    Remove comments

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,2 @@
-import sys #, subprocess
+import sys
 sys.path.append('src/')
-# subprocess.check_call([
-#     sys.executable, "-m", "pip", "install",
-#     "--no-build-isolation", "--no-cache-dir",
-#     "git+https://github.com/harvardnlp/genbmm.git",
-# ])
