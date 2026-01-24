130ef752939c289f95ee1e633b3b19234d7986cb
Grigoriy Ksenofontov <gregkseno@gmail.com>
2026-01-24 03:48:28 +0300

    Additional minor changes

--- train_hdg.sh
+++ train_hdg.sh
@@ -11,28 +11,29 @@
 sleep $((SLURM_ARRAY_TASK_ID * 5))
 source activate dot_bench
 SEED=5
+METHOD=dlight_sb # (dlight_sb cnot)
 
 case "${SLURM_ARRAY_TASK_ID}" in
   0)
     python -m src.run -m \
       hydra/launcher=submitit_local hydra.launcher.timeout_min=23040 hydra.launcher.gpus_per_node=1 \
-      hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=70 \
+      hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=80 \
       seed=${SEED} data.num_workers=1 data.pin_memory=true 'hydra.launcher.submitit_folder=${paths.log_dir}/.submitit' \
-      experiment=dlight_sb/benchmark_hdg/d2_g002,dlight_sb/benchmark_hdg/d2_g005,dlight_sb/benchmark_hdg/d2_u0005,dlight_sb/benchmark_hdg/d2_u001
+      experiment=${METHOD}/benchmark_hdg/d2_g002,${METHOD}/benchmark_hdg/d2_g005,${METHOD}/benchmark_hdg/d2_u0005,${METHOD}/benchmark_hdg/d2_u001
     ;;
   1)
     python -m src.run -m \
       hydra/launcher=submitit_local hydra.launcher.timeout_min=23040 hydra.launcher.gpus_per_node=1 \
-      hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=70 \
+      hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=80 \
       seed=${SEED} data.num_workers=1 data.pin_memory=true 'hydra.launcher.submitit_folder=${paths.log_dir}/.submitit' \
-      experiment=dlight_sb/benchmark_hdg/d16_g002,dlight_sb/benchmark_hdg/d16_g005,dlight_sb/benchmark_hdg/d16_u0005,dlight_sb/benchmark_hdg/d16_u001
+      experiment=${METHOD}/benchmark_hdg/d16_g002,${METHOD}/benchmark_hdg/d16_g005,${METHOD}/benchmark_hdg/d16_u0005,${METHOD}/benchmark_hdg/d16_u001
     ;;
   2)
     python -m src.run -m \
       hydra/launcher=submitit_local hydra.launcher.timeout_min=23040 hydra.launcher.gpus_per_node=1 \
-      hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=70 \
+      hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=80 \
       seed=${SEED} data.num_workers=1 data.pin_memory=true 'hydra.launcher.submitit_folder=${paths.log_dir}/.submitit' \
-      experiment=dlight_sb/benchmark_hdg/d64_g002,dlight_sb/benchmark_hdg/d64_g005,dlight_sb/benchmark_hdg/d64_u0005,dlight_sb/benchmark_hdg/d64_u001
+      experiment=${METHOD}/benchmark_hdg/d64_g002,${METHOD}/benchmark_hdg/d64_g005,${METHOD}/benchmark_hdg/d64_u0005,${METHOD}/benchmark_hdg/d64_u001
     ;;
   *)
     echo "Unknown SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}" >&2
