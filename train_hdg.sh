7601402cca1b4abcc6375b23769a39a4b56d8684
Grigoriy Ksenofontov <gregkseno@gmail.com>
2026-01-24 03:46:04 +0300

    Rework launch configs

--- train_hdg.sh
+++ train_hdg.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=bench_dlight_sb
-#SBATCH --partition=gpu_a100
+#SBATCH --job-name=train-hdg
+#SBATCH --partition=ais-gpu
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
-#SBATCH --array=0-2%1
+#SBATCH --array=0-2%2
 #SBATCH --nodes=1
-#SBATCH --mem=70GB
-#SBATCH --time=1-00:00:00
-#SBATCH --output=logs/bench_dlight_sb_%A_%a.out
+#SBATCH --mem=80GB
+#SBATCH --time=6-00:00:00
 
 sleep $((SLURM_ARRAY_TASK_ID * 5))
 source activate dot_bench
-SEED=42
+SEED=5
 
 case "${SLURM_ARRAY_TASK_ID}" in
   0)
@@ -19,7 +18,6 @@ case "${SLURM_ARRAY_TASK_ID}" in
       hydra/launcher=submitit_local hydra.launcher.timeout_min=23040 hydra.launcher.gpus_per_node=1 \
       hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=70 \
       seed=${SEED} data.num_workers=1 data.pin_memory=true 'hydra.launcher.submitit_folder=${paths.log_dir}/.submitit' \
-      +data.reversed=true \
       experiment=dlight_sb/benchmark_hdg/d2_g002,dlight_sb/benchmark_hdg/d2_g005,dlight_sb/benchmark_hdg/d2_u0005,dlight_sb/benchmark_hdg/d2_u001
     ;;
   1)
@@ -27,7 +25,6 @@ case "${SLURM_ARRAY_TASK_ID}" in
       hydra/launcher=submitit_local hydra.launcher.timeout_min=23040 hydra.launcher.gpus_per_node=1 \
       hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=70 \
       seed=${SEED} data.num_workers=1 data.pin_memory=true 'hydra.launcher.submitit_folder=${paths.log_dir}/.submitit' \
-      +data.reversed=true \
       experiment=dlight_sb/benchmark_hdg/d16_g002,dlight_sb/benchmark_hdg/d16_g005,dlight_sb/benchmark_hdg/d16_u0005,dlight_sb/benchmark_hdg/d16_u001
     ;;
   2)
@@ -35,7 +32,6 @@ case "${SLURM_ARRAY_TASK_ID}" in
       hydra/launcher=submitit_local hydra.launcher.timeout_min=23040 hydra.launcher.gpus_per_node=1 \
       hydra.launcher.tasks_per_node=1 hydra.launcher.cpus_per_task=2 hydra.launcher.mem_gb=70 \
       seed=${SEED} data.num_workers=1 data.pin_memory=true 'hydra.launcher.submitit_folder=${paths.log_dir}/.submitit' \
-      +data.reversed=true \
       experiment=dlight_sb/benchmark_hdg/d64_g002,dlight_sb/benchmark_hdg/d64_g005,dlight_sb/benchmark_hdg/d64_u0005,dlight_sb/benchmark_hdg/d64_u001
     ;;
   *)
