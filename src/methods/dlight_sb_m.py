134a6612cdd4e2e87419d76b940ac17712c30099
Grigoriy Ksenofontov <gregkseno@gmail.com>
2026-01-24 03:57:26 +0300

    Fix optimizer initialization

--- src/methods/dlight_sb_m.py
+++ src/methods/dlight_sb_m.py
@@ -341,7 +341,7 @@ class DLightSB_M(LightningModule):
 
     def configure_optimizers(self) -> List[Dict[str, Any]]:
         optimizer = self.hparams.optimizer(
-            params=[self.log_alpha] + list(self.log_cp_cores)
+            params=[self.log_alpha, self.log_cp_cores]
         )
         if self.hparams.scheduler is not None:
             scheduler = self.hparams.scheduler(optimizer=optimizer)
