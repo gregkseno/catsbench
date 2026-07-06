eb38a4c7c80ab902689836e9e98e308e3d8a8070
Grigoriy Ksenofontov <gregkseno@gmail.com>
2026-07-06 17:16:05 +0300

    Fix reuse of the port

--- scripts/train_celeba.sh
+++ scripts/train_celeba.sh
@@ -15,6 +15,14 @@ SEED=5
 EXPERIMENTS=(u0005 u001)
 EXPERIMENT="${EXPERIMENTS[$SLURM_ARRAY_TASK_ID]}"
 
+if [ -n "${SLURM_ARRAY_TASK_ID:-}" ]; then
+  BASE=30000
+  OFFSET=$(( SLURM_ARRAY_TASK_ID % 1000 ))
+  export MASTER_PORT=$(( BASE + OFFSET ))
+else
+  export MASTER_PORT=29500
+fi
+
 python -m src.run \
   seed=${SEED} data.num_workers=2 data.pin_memory=true \
   experiment=alpha_csbm/celeba/${EXPERIMENT}
--- scripts/train_colored_mnist.sh
+++ scripts/train_colored_mnist.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=ais-gpu
 #SBATCH --gpus=2
 #SBATCH --cpus-per-task=8
-#SBATCH --array=0-5%2
+#SBATCH --array=1-5%2
 #SBATCH --nodes=1
 #SBATCH --mem=80GB
 #SBATCH --time=6-00:00:00
@@ -14,6 +14,14 @@ source activate dot_bench
 SEED=5
 EXPERIMENTS=(t2 t4 t10 t25 t50 t100)
 EXPERIMENT="${EXPERIMENTS[$SLURM_ARRAY_TASK_ID]}"
+
+if [ -n "${SLURM_ARRAY_TASK_ID:-}" ]; then
+  BASE=30000
+  OFFSET=$(( SLURM_ARRAY_TASK_ID % 1000 ))
+  export MASTER_PORT=$(( BASE + OFFSET ))
+else
+  export MASTER_PORT=29500
+fi
 
 python -m src.run \
   seed=${SEED} data.num_workers=4 data.pin_memory=true \
